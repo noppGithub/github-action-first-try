c1a6fc4d95cda11a2723d77038a8da6f7ba284cc
nnopp <n.phantawee@gmail.com>
2022-02-18 13:46:55 +0700

    added numpy and requirements.txt

--- test_mycode.py
+++ test_mycode.py
@@ -1,7 +1,14 @@
 from mycode import Example
+import numpy as np
 
 
 def test_hello():
     c = Example()
     text = "Dew"
     c.hello(text)
+
+
+def test_numpy():
+    x = [1, 3, 5]
+    output = np.max(x)
+    assert output == 3
